4d022db0f052e9b7bd6c8895c88317b72f5a0448
Mark Schisler <mark.schisler@gmail.com>
2026-09-03 16:36:37 -0500

    cleanup faq

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -18,3 +18,18 @@ for (var i = 0; i < menuLinks.length; i++) {
         }
     });
 }
+
+var faqQuestions = document.querySelectorAll('.faq-question');
+for (var j = 0; j < faqQuestions.length; j++) {
+    faqQuestions[j].addEventListener('click', function() {
+        var item = this.closest('.faq-item');
+        var isOpen = item.classList.toggle('open');
+        this.setAttribute('aria-expanded', isOpen);
+    });
+    faqQuestions[j].addEventListener('keydown', function(e) {
+        if (e.key === 'Enter' || e.key === ' ') {
+            e.preventDefault();
+            this.click();
+        }
+    });
+}
